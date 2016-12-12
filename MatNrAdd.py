acb4cac47460564b6696e54db6d66a6b22a14716
Acrasy <sai.whocares@gmail.com>
2016-12-12 22:48:25 +0100

    first commit, basic files

---
+++ MatNrAdd.py
@@ -0,0 +1,14 @@
+import os
+
+with open("./pw-list/am-pass-full.txt","r") as list:
+
+	table = list.read().strip().split()
+	for i in range(len(table)):
+		table[i] = table[i] + "1225253"
+
+	with open('./pw-list/MatNr-pass.txt', mode='wt', encoding='utf-8') as myfile:
+		myfile.write('\n'.join(table))
+
+
+
+
